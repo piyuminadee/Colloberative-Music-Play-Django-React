12d1cb0872b91c197a92655f249cde3c638643cd
Nadeesh Weerarathna <nadeesh-cs20024@stu.kln.ac.lk>
2024-10-25 15:25:47 +0530

    build component

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -1,5 +1,8 @@
 import React, {Component} from "react";
-import {render} from "react-dom";
+import { createRoot } from "react-dom/client";
+import HomePage from "./HomePage";
+
+
 
 export default class App extends Component{
     constructor(props){
@@ -7,9 +10,18 @@ export default class App extends Component{
     }
 
     render(){
-        return <h1>Testing React Code</h1>;
+        return(
+            <div>
+            <HomePage />
+          
+            
+         </div>
+        );
+         
+        
     }
 }
 
 const appDiv = document.getElementById("app");
-render(<App />, appDiv);
+const root = createRoot(appDiv); // Create a root
+root.render(<App />);
